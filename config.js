4a8093053bc2745515ef6aac221280b98fc55078
sheikhfaisal12 <141120176+sheikhfaisal12@users.noreply.github.com>
2023-09-24 19:59:46 +0500

    Update config.js

--- config.js
+++ config.js
@@ -17,15 +17,15 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["8801853262586","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801853262586","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801928502256"];
-global.OwnerName = "Slasher";
-global.BotName = "XLICON-MD";
+global.Owner = ["923433496253","923433496253""];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ["923433496253"","923433496253""];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.ownertag = ["923433496253""];
+global.OwnerName = "sheikh faisal";
+global.BotName = "Sheikh-faisal";
 global.packname = "X-2.0 Bot";                             //Do not change.
-global.author = "By: Slasher";                               //Do not change.
+global.author = "By: Sheikh";                               //Do not change.
 global.BotSourceCode = "https://github.com/ahil15/Xlicon-v2"; //Do not change.
-global.SupportGroupLink = "https://chat.whatsapp.com/EjsQvJNcFGVCSfaBEIxZm2"; 
+global.SupportGroupLink = "https://chat.whatsapp.com/CletLyGqM5Z210HjZAMzDz"; 
 
 
 //
